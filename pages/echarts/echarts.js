e16d63224f4d64c2082ccfb8921d8716c8875ede
Ureumono <870665934@qq.com>
2019-07-12 12:45:15 +0800

    echarts动态更新实验

--- pages/echarts/echarts.js
+++ pages/echarts/echarts.js
@@ -3,70 +3,23 @@ import * as echarts from '../../ec-canvas/echarts';
 
 const app = getApp();
 
-function initChart(canvas, width, height) {
-  const chart = echarts.init(canvas, null, {
-    width: width,
-    height: height
-  });
-  canvas.setChart(chart);
+var Chart = null;
 
-  var option = {
-    title: {
-      text: '测试下面legend的红色区域不应被裁剪',
-      left: 'center'
-    },
-    color: ["#37A2DA", "#67E0E3", "#9FE6B8"],
-    legend: {
-      data: ['A', 'B', 'C'],
-      top: 50,
-      left: 'center',
-      backgroundColor: 'white',
-      z: 100
-    },
-    grid: {
-      containLabel: true
-    },
-    tooltip: {
-      show: true,
-      trigger: 'axis'
-    },
-    xAxis: {
-      type: 'category',
-      boundaryGap: false,
-      data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
-      // show: false
-    },
-    yAxis: {
-      x: 'center',
-      type: 'value',
-      splitLine: {
-        lineStyle: {
-          type: 'dashed'
-        }
-      }
-      // show: false
-    },
-    series: [{
-      name: 'A',
-      type: 'line',
-      smooth: true,
-      data: [18, 36, 65, 30, 78, 40, 33]
-    }, {
-      name: 'B',
-      type: 'line',
-      smooth: true,
-      data: [12, 50, 51, 35, 70, 30, 20]
-    }, {
-      name: 'C',
-      type: 'line',
-      smooth: true,
-      data: [10, 30, 31, 50, 40, 20, 10]
-    }]
-  };
+// function initChart (canvas, width, height) {
+//   const chart = echarts.init(canvas, null, {
+//     width: width,
+//     height: height
+//   });
+//   canvas.setChart(chart);
 
-  chart.setOption(option);
-  return chart;
-}
+//   var option = {
+    
+    
+//   };
+
+//   chart.setOption(option);
+//   return chart;
+// }
 
 Page({
 
@@ -75,15 +28,99 @@ Page({
    */
   data: {
     ec: {
-      onInit: initChart
+      lazyLoad: true
+    },
+    data1: null,
+    data2: null
+  },
+
+  getData: function () {
+    this.data.data1 = [18, 36, 65, 30, 78, 40, 33]
+    this.data.data2 = [12, 50, 51, 35, 70, 30, 20]
+    if (!Chart) {
+      this.init_echarts(); //初始化图表
+    } else {
+      this.setOption(Chart); //更新数据
     }
+  },
+
+  init_echarts: function () {
+    this.echartsComponnet.init((canvas, width, height) => {
+      // 初始化图表
+      Chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+      });
+      // Chart.setOption(this.getOption());
+      this.setOption(Chart);
+      // 注意这里一定要返回 chart 实例，否则会影响事件处理等
+      return Chart;
+    });
+  },
+
+  setOption: function (Chart) {
+    Chart.clear();  // 清除
+    Chart.setOption(this.getOption());  //获取新数据
+  },
+
+  getOption: function () {
+    var option = {
+      title: {
+        text: '测试下面legend的红色区域不应被裁剪',
+        left: 'center'
+      },
+      color: ["#37A2DA", "#67E0E3", "#9FE6B8"],
+      legend: {
+        data: ['A', 'B', 'C'],
+        top: 50,
+        left: 'center',
+        backgroundColor: 'white',
+        z: 100
+      },
+      grid: {
+        containLabel: true
+      },
+      tooltip: {
+        show: true,
+        trigger: 'axis'
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+        // show: false
+      },
+      yAxis: {
+        x: 'center',
+        type: 'value',
+        splitLine: {
+          lineStyle: {
+            type: 'dashed'
+          }
+        }
+        // show: false
+      },
+      series: [{
+        name: 'A',
+        type: 'line',
+        smooth: true,
+        data: this.data.data1
+      }, {
+        name: 'B',
+        type: 'line',
+        smooth: true,
+        data: this.data.data2
+      }]
+    }
+    return option;
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.echartsComponnet = this.selectComponent('#mychart-dom-line');
+    this.getData();
   },
 
   /**
