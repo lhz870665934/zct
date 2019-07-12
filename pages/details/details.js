ca7e834f0a070e810f0c2a86d7c792f7061cfdbf
Ureumono <870665934@qq.com>
2019-07-12 16:49:32 +0800

    产品详情页面初步

---
+++ pages/details/details.js
@@ -0,0 +1,68 @@
+// pages/details/details.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    product_name: "易达方上证50指数C（003333）",
+    earning: 2.47,
+    benefit: 0.6481,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
