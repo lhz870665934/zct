aa393fed5c8c5b24ed53a38d8c9980470ecdb40c
Ureumono <870665934@qq.com>
2019-07-15 09:53:50 +0800

    双纵坐标尝试

--- pages/echarts/echarts.js
+++ pages/echarts/echarts.js
@@ -5,22 +5,6 @@ const app = getApp();
 
 var Chart = null;
 
-// function initChart (canvas, width, height) {
-//   const chart = echarts.init(canvas, null, {
-//     width: width,
-//     height: height
-//   });
-//   canvas.setChart(chart);
-
-//   var option = {
-    
-    
-//   };
-
-//   chart.setOption(option);
-//   return chart;
-// }
-
 Page({
 
   /**
@@ -36,7 +20,7 @@ Page({
 
   getData: function () {
     this.data.data1 = [18, 36, 65, 30, 78, 40, 33]
-    this.data.data2 = [12, 50, 51, 35, 70, 30, 20]
+    this.data.data2 = [520, 600, 501, 505, 700, 600, 800]
     if (!Chart) {
       this.init_echarts(); //初始化图表
     } else {
@@ -90,7 +74,7 @@ Page({
         data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
         // show: false
       },
-      yAxis: {
+      yAxis: [{
         x: 'center',
         type: 'value',
         splitLine: {
@@ -100,15 +84,28 @@ Page({
         }
         // show: false
       },
+        {
+          x: 'center',
+          type: 'value',
+          splitLine: {
+            lineStyle: {
+              type: 'dashed'
+            }
+          }
+          // show: false
+        }
+      ],
       series: [{
         name: 'A',
         type: 'line',
         smooth: true,
+        yAxisIndex: 0,
         data: this.data.data1
       }, {
         name: 'B',
         type: 'line',
         smooth: true,
+        yAxisIndex: 1,
         data: this.data.data2
       }]
     }
