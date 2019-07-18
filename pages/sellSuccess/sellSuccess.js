a5849b7574699a61ab85af734e3fa7c49e9698d9
Ureumono <870665934@qq.com>
2019-07-18 17:47:51 +0800

    卖出成功界面完成

--- pages/sellSuccess/sellSuccess.js
+++ pages/sellSuccess/sellSuccess.js
@@ -1,50 +1,16 @@
-// pages/sell/sell.js
+// pages/sellSuccess/sellSuccess.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    all_amount: 998,
-    amount: null,
-    isOverflow: false,
-    finalDate: "7月27日(三日后)23:59",
-    today: "7月24日",
-    finalDay: "7月27日"
+
   },
 
-  bindAmountValue: function (e) {
-    this.setData({
-      amount: e.detail.value
-    })
-    if (this.data.amount > this.data.all_amount) {
-      this.setData({
-        isOverflow: true
-      })
-    }
-    else {
-      this.setData({
-        isOverflow: false
-      })
-    }
-  },
-
-  setAll: function () {
-    this.setData({
-      amount: this.data.all_amount
-    })
-  },
-
-  clear: function () {
-    this.setData({
-      amount: null,
-      isOverflow: false
-    })
-  },
-
-  confirm: function () {
-    wx.navigateTo({
-      url: "../sellSuccess/sellSuccess",
+  finish: function () {
+    wx.navigateBack({
+      delta: 2
     })
   },
 
