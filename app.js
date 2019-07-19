d7f030d688e6d753d046013db251237953775ff6
Ureumono <870665934@qq.com>
2019-07-19 18:00:45 +0800

    授权问题修复，加入授权页面

--- app.js
+++ app.js
@@ -6,9 +6,29 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    wx.login({
-      
-    })
+    // wx.getSetting({
+    //   success(res) {
+    //     if (!res.authSetting['scope.userInfo']) {
+    //       wx.authorize({
+    //         scope: 'scope.userInfo',
+    //         success() {
+    //           wx.getUserInfo({
+    //             success: res => {
+    //               // 可以将 res 发送给后台解码出 unionId
+    //               this.globalData.userInfo = res.userInfo
+
+    //               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //               // 所以此处加入 callback 以防止这种情况
+    //               if (this.userInfoReadyCallback) {
+    //                 this.userInfoReadyCallback(res)
+    //               }
+    //             }
+    //           })
+    //         }
+    //       })
+    //     }
+    //   }
+    // })
 
     // 登录
     // wx.login({
@@ -29,9 +49,10 @@ App({
     //     // 发送 res.code 到后台换取 openId, sessionKey, unionId
     //   }
     // })
-    // 获取用户信息
+    //获取用户信息
     wx.getSetting({
       success: res => {
+        console.log(res.authSetting)
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
--- pages/index/index.js
+++ pages/index/index.js
@@ -207,6 +207,29 @@ Page({
   },
 
   onLoad: function () {
+    wx.getSetting({
+      success: res => {
+        console.log(res.authSetting)
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              app.globalData.userInfo = res.userInfo
+              this.onShow();
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+                this.onShow();
+              }
+            }
+          })
+        }
+      }
+    })
+
+
     console.log("test")
     var that = this;
     wx.getSystemInfo({
@@ -257,10 +280,9 @@ Page({
         })
       }
     }
+  },
 
-
-    console.log("show");
-    // this.wechatLogin()
+  onShow: function () {
     var that = this;
     if (app.globalData.userInfo) {
       this.setData({
@@ -271,19 +293,16 @@ Page({
     } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
-      console.log("fuck")
       console.log(app.globalData.opneid)
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
         })
-        console.log("2")
         this.wechatLogin()
       }
     } else {
       // 在没有 open-type=getUserInfo 版本的兼容处理
-      console.log("3")
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
@@ -291,25 +310,20 @@ Page({
             userInfo: res.userInfo,
             hasUserInfo: true
           })
-          console.log("4")
           this.wechatLogin()
         }
       })
     }
   },
 
-  onShow: function () {
-    
-  },
-
-  getUserInfo: function(e) {
-    console.log(e)
-    // console.log("test")
-    this.sendOpenid()
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
+  // getUserInfo: function(e) {
+  //   console.log(e)
+  //   // console.log("test")
+  //   this.sendOpenid()
+  //   app.globalData.userInfo = e.detail.userInfo
+  //   this.setData({
+  //     userInfo: e.detail.userInfo,
+  //     hasUserInfo: true
+  //   })
+  // }
 })
